face513dbe019ef8805ae57bdcbed5efbcbfaffc
btalley07 <81830387+btalley07@users.noreply.github.com>
2021-06-29 13:20:05 -0700

    JS DAY 2 WEEK 6
    JS DAY 2 WEEK 6

--- main.js
+++ main.js
@@ -1,99 +1,88 @@
-//==================Exercise #1 ==========//
-/*Write a function that takes in the string and the list of dog names, loops through 
-the list and checks that the current name is in the string passed in. 
-The output should be:
-"Matched dog_name" if name is in the string, if no matches are present 
-console.log "No Matches"
-*/
-let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
-let dog_names = ["Max","HAS","PuRple","dog"]
-
-function findWords(){
-    for(let i = 0; i < dog_names.length +1; i++){
-        if(dog_string.toLowerCase().includes(dog_names[i].toLowerCase())){
-            console.log(dog_names[i])
-            
-        } else{
-            return 'no matches'
-        }
-    }
-    return dog_names
-}
-    
-console.log(findWords(dog_string,dog_names))
-//Call method here with parameters
-
-//============Exercise #2 ============//
-/*Write a fucntion that takes in an array and removes every even index with a splice,
-and replaces it with the string "even index" */
-
-names = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
-
-function replaceEvens(arr){
-    for(let i = 0; i < names.length; i++){
-        if(i % 2 ==0){
-            names.splice(i,1,'even index')
-        }
-    }
-    return names
-}
-console.log(replaceEvens())
-
-//Expected output
-//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
-//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
-
-
-
-
-//CODEWARS PROBLEM 1
+//----- Exercise #1
 /*
-    Complete the function that accepts a string parameter, 
-    and reverses each word in the string. 
-    All spaces in the string should be retained.
+Write a function that parses through the below object and displays 
+all of their
+favorite food dishes as shown:
 */
 
-//python answer
-// def reverse_words(text):
-//     return ' '.join(word[::-1] for word in text.split(' '))
-
-//JS answer
-let reverse_words = (str) => str.split('').reverse().join('');
-console.log(reverse_words(`This is an example!`));
-
-
-
-
-//CODEWARS PROBLEM 2
-/* 
-    Task
-Given an integral number, determine if it's a square number:
-*/
-
-//PYTHON ANSWER
-/*
-   def is_square(n):       
-    if n < 0:
-        return False
-    elif n == 0:
-        return True
-    i = 1
-    while i < n:
-        m = i * i
-        if m == n:
-            return True
-        elif m > n:
-            return False
-        i += 1
-
-    return False
-*/
-
-// //JS ANSWER
-var is_square = function(n){
-     if(n === 0){
-    
-     return true} else {
-     return n > 0 && Math.sqrt(n) % 1 === 0;}
+let person3 = {
+    pizza:["Deep Dish","South Side Thin Crust"],
+    tacos:"Anything not from Taco bell",
+    burgers:"Portillos Burgers",
+    ice_cream:["Chocolate","Vanilla","Oreo"],
+    shakes:[{
+        oberwise:"Chocolate",
+        dunkin:"Vanilla",
+        culvers:"All of them",
+        mcDonalds:"Sham-rock-shake",
+        cupids_candies:"Chocolate Malt"
+    }]
 }
-console.log(is_square)
+console.log(Object.keys(person3))
+console.log(Object.values(person3))
+
+//=======Exercise #2=========//
+/*
+Write an object prototype for a Person that has a name and age, has a
+printInfo method, and also has a method that 
+increments the persons age by 1 each time the method is called.
+Create two people using the 'new' keyword, and print 
+both of their infos and increment one persons
+age by 3 years. Use an arrow function for both methods
+*/
+
+// Create our Person Prototype
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+
+    this.printInfo = function(hair_color, weight){
+        console.log(`My name is ${'this.name'}, I am ${'this.age'}
+        and have ${hair_color} and weigh ${weight} pounds`);
+
+        return 'returned value'
+    }
+}
+let peep = new Person('Blair', 32)
+console.log(peep.printInfo('purple', 'unknown'))
+// Create another arrow function for the addAge method that takes a single parameter
+
+// Adding to the age 
+
+
+// =============Exercise #3 ============//
+/*
+
+    Create a Promised based function that will check a string to determine if 
+    it's length is greater than 10.
+    If the length is greater than ten console log "Big word". 
+    If the length of the string is less than 10 console log "Small Number"
+*/
+function bigWord(){
+    const words = 'some awesome words here';
+    if(words > 10) {
+        result = 'big words';
+    }else{
+        result = 'small numbers'
+    }
+
+}
+console.log(bigWord)
+//CODEWARS #1
+//Summation
+//Write a program that finds the summation of every number from 1 to num. 
+//The number will always be a positive integer greater than 0.
+
+function summation(num) {
+    return num * (num + 1) / 2
+  }
+
+
+//CODEWARS #2
+function even_or_odd(number) {
+    if (number%2 == 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
